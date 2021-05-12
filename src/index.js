e9e25c02d9f97cbe96e4caaffb18558a449421aa
Pratik <shivam3448@gmail.com>
2021-05-12 08:17:23 +0530

    Working with React router dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/App";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+// import App from "./component/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function NoMatch() {
+  return <h2>404</h2>
+}
